c31f60475b6b8bfa1ffffa3ec7689a47f5375bfc
Dotas <lyallstorm@gmail.com>
2025-05-22 12:56:00 +0200

    Adding nitro-cli polygonID docker image and using it from enclave

--- enclaver/src/build.rs
+++ enclaver/src/build.rs
@@ -19,7 +19,7 @@ use uuid::Uuid;
 const ENCLAVE_OVERLAY_CHOWN: &str = "0:0";
 const RELEASE_OVERLAY_CHOWN: &str = "0:0";
 
-const NITRO_CLI_IMAGE: &str = "registry.edgebit.io/nitro-cli:latest";
+const NITRO_CLI_IMAGE: &str = "privadoid/aws-nitro-cli:1.4.2";
 const ODYN_IMAGE: &str = "registry.edgebit.io/odyn:latest";
 const ODYN_IMAGE_BINARY_PATH: &str = "/usr/local/bin/odyn";
 const RELEASE_BASE_IMAGE: &str = "registry.edgebit.io/enclaver-wrapper-base:latest";
